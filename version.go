85715a464642eb0c4b677a4cbb77272cc0856a41
Fx <vinhhp@vhttech.com>
2026-05-12 02:52:40 +0000

    change brand and repo

--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 /*
- * Bamboo - A Vietnamese Input method editor
+ * vhtime - A Vietnamese Input method editor
  * Copyright (C) 2018 Luong Thanh Lam <ltlam93@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
